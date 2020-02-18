ef1188dc7a5fa08493733b4b46988d729458f88b
Luce Carter <luce@lucyscarter.co.uk>
2020-02-18 22:15:07 +0000

    Added slider for count increments. Attempting binding label value to slider value

--- Counter/MainPage.xaml.cs
+++ Counter/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -14,14 +15,38 @@ namespace Counter
     public partial class MainPage : ContentPage
     {
         private int count = 0;
+        
+
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
+        }
+
+        private int _sliderValue;
+        public int SliderValue {
+            get => _sliderValue;
+            set
+            {
+                if ( value != _sliderValue)
+                {
+                    _sliderValue = value;
+                    NotifyPropertyChanged("Slider");
+                }
+            }
+        }       
+
+        public string SliderLabel => SliderValue.ToString();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
 
         private void IncrementCounterClicked(object sender, EventArgs e)
         {
-            count++;
+            count += SliderValue;
             CounterLabel.Text = count.ToString();
         }
     }
